8a1b900a88b997045199d32376b0d7621db93dfe
mgoelzer <mike@goelzer.com>
2016-01-14 11:07:28 -0800

    All containers should have 'unless-stopped' restart policy

--- Vagrant/after-reboot.sh
+++ Vagrant/after-reboot.sh
@@ -4,19 +4,19 @@
 ## Commands to create the Swarm layer containers
 ##
 
-docker -H=tcp://192.168.33.11:2375 run -d -p 8500:8500 -h consul progrium/consul -server -bootstrap
+docker -H=tcp://192.168.33.11:2375 run --restart=unless-stopped -d -p 8500:8500 -h consul progrium/consul -server -bootstrap
 
-docker -H=tcp://192.168.33.20:2375 run -d swarm join --advertise=192.168.33.20:2375 consul://192.168.33.11:8500/
-docker -H=tcp://192.168.33.21:2375 run -d swarm join --advertise=192.168.33.21:2375 consul://192.168.33.11:8500/
+docker -H=tcp://192.168.33.20:2375 run --restart=unless-stopped -d swarm join --advertise=192.168.33.20:2375 consul://192.168.33.11:8500/
+docker -H=tcp://192.168.33.21:2375 run --restart=unless-stopped -d swarm join --advertise=192.168.33.21:2375 consul://192.168.33.11:8500/
 [et cetera]
 
-docker -H=tcp://192.168.33.200:2375 run -d swarm join --advertise=192.168.33.200:2375 consul://192.168.33.11:8500/
-docker -H=tcp://192.168.33.201:2375 run -d swarm join --advertise=192.168.33.201:2375 consul://192.168.33.11:8500/
+docker -H=tcp://192.168.33.200:2375 run --restart=unless-stopped -d swarm join --advertise=192.168.33.200:2375 consul://192.168.33.11:8500/
+docker -H=tcp://192.168.33.201:2375 run --restart=unless-stopped -d swarm join --advertise=192.168.33.201:2375 consul://192.168.33.11:8500/
 [et cetera]
 
-docker -H=tcp://192.168.33.250:2375 run -d swarm join --advertise=192.168.33.250:2375 consul://192.168.33.11:8500/
+docker -H=tcp://192.168.33.250:2375 run --restart=unless-stopped -d swarm join --advertise=192.168.33.250:2375 consul://192.168.33.11:8500/
 
-docker -H=tcp://192.168.33.11:2375 run -d -p 3375:2375 swarm manage consul://192.168.33.11:8500/
+docker -H=tcp://192.168.33.11:2375 run --restart=unless-stopped -d -p 3375:2375 swarm manage consul://192.168.33.11:8500/
 
 export DOCKER_HOST="tcp://192.168.33.11:3375"
 docker network create --driver overlay mynet
